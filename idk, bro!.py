6a2060426b272c2572c71de64c29383d8ac468d2
mrgamer <heitoruliana4@gmail.com>
2026-04-09 19:02:06 -0300

    Add files via upload

---
+++ idk, bro!.py
@@ -0,0 +1,15 @@
+temperature = 32
+
+b = 20
+if temperature > b:
+    result = temperature - b
+
+
+if result >= 30:
+    print("is the Ricardo!")
+
+elif result <= 20:
+    print("victim is here...")
+
+else:
+    print("is the Ricardo, but not the real one!")
